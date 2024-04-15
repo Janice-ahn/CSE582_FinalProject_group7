0ebc1426e57af6e1398df6b99ab65156df85d0d5
Wenchao-M <2020202110053@whu.edu.cn>
2024-04-15 00:18:12 -0400

    add ablations on mt5 model

--- mt5/run.sh
+++ mt5/run.sh
@@ -2,6 +2,7 @@ gpu=$1
 batch=$2
 model=$3
 lr=$4  # 5e-4 for small, base and large; 1e-4 for 3b
+ablation_mode=$5
 
 export out_dir="./out/cls/${model}"
 export data_dir="./data"
@@ -16,7 +17,7 @@ export CUDA_LAUNCH_BLOCKING="0"
 export epoch=5
 export lr_cls=3e-3
 
-python main.py \
+python ablation.py \
     --model_name_or_path ${model} \
     --do_train \
     --do_predict \
@@ -24,6 +25,7 @@ python main.py \
     --test_file ${data_dir}/test.csv \
     --per_device_train_batch_size ${batch} \
     --per_device_eval_batch_size ${batch} \
+    --ablation_mode ${ablation_mode} \
     --cache_dir ${cache_dir} \
     --output_dir ${out_dir} \
     --learning_rate ${lr} \
@@ -34,3 +36,4 @@ python main.py \
     --max_seq_length 1024 \
     --classifier_dropout 0.2 \
     --label_column_name label
+
